ba9f35f1aa01245cb958142715d6d95f9d4a0b9d
3S00mc <3_S00mc@proton.me>
2025-07-26 11:42:46 -0300

    Add files via upload

---
+++ Projeto002_NumeroSecreto/app.js
@@ -0,0 +1,30 @@
+// alert("Bem vindo ao jogo do número secreto!");
+//  let numeroSecreto = 42;
+//  let chute = prompt("Digite um número entre 1 e 100");
+
+//  if(chute == numeroSecreto) {
+//     // alert("Parabéns! Você acertou o número secreto!");
+//     console.log("Parabéns! Você acertou o número secreto!");
+//  }
+
+//SEGUNDO CODIGO
+// let mensagemBoasVindas =  "Bem vindo ao App!";
+// alert(mensagemBoasVindas);
+
+//TERCEIRO CODIGO
+// alert("Bem vindo ao jogo do número secreto!")
+// let chute = prompt("Escolha um número entre 1 e 10")
+// let numeroSecreto = 4
+// if(chute == numeroSecreto) {
+//     alert("Acertou")
+// }
+
+// QUARTO CODIGO
+
+let senhaDoSistema = "senhaTeste!"
+
+let senha = prompt("Digite a senha do sistema:")
+
+if (senha == senhaDoSistema){
+    alert("Acesso ao sistema garantido")
+}
