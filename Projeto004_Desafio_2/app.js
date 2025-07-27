ac8190132babff35cae7f924611d36e08cd6e670
Pedro Luiz <3_S00mc@proton.me>
2025-07-26 22:35:16 -0300

    Add files via upload

--- Projeto004_Desafio_2/app.js
+++ Projeto004_Desafio_2/app.js
@@ -1,32 +1,116 @@
-// Projeto004_Desafio_2/app.js
+// alert("Bem vindo ao jogo do número secreto!");
+//  let numeroSecreto = 42;
+//  let chute = prompt("Digite um número entre 1 e 100");
 
-//CODIGO 1 - DIA DA SEMANA
-// let nomeUsuario = prompt("Bom dia! Qual é o seu nome?");
-// let diaSemana = prompt("Oi " + nomeUsuario + ", que dia da semana é hoje?");
+//  if(chute == numeroSecreto) {
+//     // alert("Parabéns! Você acertou o número secreto!");
+//     console.log("Parabéns! Você acertou o número secreto!");
+//  }
 
-// if(diaSemana === "Sábado" || diaSemana === "Domingo") {
-//     alert("Bom final de semana!");
-// }else{
-//     alert("Está muito longe para o final de semena????");
+
+//SEGUNDO CODIGO
+// let mensagemBoasVindas =  "Bem vindo ao App!";
+// alert(mensagemBoasVindas);
+
+
+//TERCEIRO CODIGO
+// alert("Bem vindo ao jogo do número secreto!")
+// let chute = prompt("Escolha um número entre 1 e 10")
+// let numeroSecreto = 4
+// if(chute == numeroSecreto) {
+//     alert("Acertou")
 // }
-//O código roda se eu clicar em cancelar.... e pega valores em branco.
 
 
-//CODIGO 2 - VALORES POSITIVOS OU NEGATIVOS
-// let numero = prompt ("Digite um número qualquer:");
-// if (numero >= 0){
-//     alert ("O número " + numero + " é positivo!");
-// }else{
-//     alert("O número " + numero + " é negativo!");
+// QUARTO CODIGO
+// let senhaDoSistema = "senhaTeste!"
+
+// let senha = prompt("Digite a senha do sistema:")
+
+// if (senha == senhaDoSistema){
+//     alert("Acesso ao sistema garantido")
 // }
 
-//CODIGO 3 - SALDO NA CONTA
-let saldo = 1000;
-let valorSaque = prompt(`Seu saldo é de R$ ${saldo} Qual o valor quer retirar?`);
-if (valorSaque <= saldo){
-    saldo -= valorSaque;
-    alert(`Saque realizado com sucesso! Seu novo saldo é de R$ ${saldo}`);
-}else{
-    alert("Saldo insuficiente para realizar o saque!");
-    alert(`Seu saldo atual é de R$ ${saldo}`);
-}
+
+// QUINTO CODIGO
+// alert("Bem vindo ao jogo do número secreto!");
+
+// let numeroSecreto = 42;
+// let chute = prompt("Digite um número entre 1 e 100");
+
+// console.log(numeroSecreto);
+
+// if (chute == numeroSecreto){
+//     alert(`Parabéns! Você acertou o número secreto [ ${numeroSecreto} ]`);
+// }else{
+//     alert("Que pena! Você errou.");
+// }
+
+
+// SEXTO CODIGO
+// alert("Bem vindo ao jogo do número secreto!");
+// let numeroSecreto = 42;
+// let chute = prompt("Digite um número entre 1 e 100");
+
+// if (chute == numeroSecreto) {
+//     alert(`Parabéns! Você acertou, o número é [ ${numeroSecreto} ]`);
+// } else {
+//         if (chute > numeroSecreto) {
+//             alert("O número secreto é menor. Tente novamente!");
+//         } else {
+//             alert("O número secreto é maior. Tente novamente!");
+//         }
+// }  
+
+
+// // SÉTIMO CODIGO
+// alert("Bem vindo ao jogo do número secreto!");
+
+// let numeroSecreto = 42;
+// let chute;
+
+// while (chute != numeroSecreto) {
+//     chute = prompt("Digite um número entre 1 e 100");
+//     if (chute == numeroSecreto) {
+//         alert("Parabéns! Você acertou o número secreto!");
+//     } else {
+//         if (chute > numeroSecreto) {
+//             alert("O número secreto é menor. Tente novamente!");
+//         }   else {
+//             alert("O número secreto é maior. Tente novamente!");
+//         }
+//     }
+// }
+
+
+// OITAVO CODIGO
+alert("Bem vindo ao jogo do número secreto!");
+
+let numeroMinimo = 1; // Define o número mínimo.... nao esta funcionando direito.
+let numeroMaximo = 10; // Define o número máximo
+let numeroSecreto = parseInt(Math.random() * numeroMaximo + 1); // Gera um número aleatório entre o Minimo e o Maximo
+let chute;
+let tentativas = 1;
+
+while (chute != numeroSecreto) {
+    chute = prompt(`Digite um número entre ${numeroMinimo} e ${numeroMaximo}`);
+    if (chute == numeroSecreto) {
+        break; // Sai do loop se o chute estiver correto
+    } else {
+        if (chute > numeroSecreto) {
+            alert("O número secreto é menor. Tente novamente!");
+        } else {
+            alert("O número secreto é maior. Tente novamente!");
+        }
+        tentativas++;
+    }
+}       
+
+    let palavraTentativas = tentativas > 1 ? "tentativas" : "tentativa";
+    alert(`Você acertou o número secreto [ ${numeroSecreto} ] em ${tentativas} ${palavraTentativas} !`);
+
+    // if (tentativas == 1) {
+    //     alert("Nossa! Acertou de primeira !!!! Você é vidente?");
+    // } else {
+    //     alert(`Você acertou o número secreto [ ${numeroSecreto} ] em ${tentativas} tentativas!`);
+    // }
